3b320e63d109a0994ff4d6a60ec3349c13edfe5c
CloudyServices <info@cloudyservices.co.uk>
2017-06-24 17:17:52 +0100

    this is better

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-var request = require('request');
 var builder = require('botbuilder');
+var restify = require('restify');
 var apiairecognizer = require('api-ai-recognizer');
-var connector = new builder.ConsoleConnector().listen();
+var request = require('request');
+//========================================================= 
+// Bot Setup 
+//========================================================= 
+// Setup Restify Server 
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
+// Create chat bot 
+var connector = new builder.ChatConnector({
+    appId: e02a9235-4dda-41f3-b756-d3055cd39a22,
+    appPassword: hWNEV3JcqF37L8xFuTXo2Lv });
 var bot = new builder.UniversalBot(connector);
 var recognizer = new apiairecognizer('ed7772a94ebd4ab09d84792bbecd9693');
 var intents = new builder.IntentDialog({recognizers: [recognizer] });

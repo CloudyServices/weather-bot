8ab0636cca2675c8bd6f807156998a8512cfd0fd
CloudyServices <info@cloudyservices.co.uk>
2017-07-01 15:03:27 +0100

    Version 0.88

--- index.js
+++ index.js
@@ -70,10 +70,10 @@ intents.matches('whatisWeatherForecast', [
             request(url, function (error, response, body) {
                 body = JSON.parse(body);
                 city_proper = body.location.name;
-                dateday1 = body.forecast.forecastday.date;
-                tempday1 = body.forecast.forecastday.day.maxtemp_c;
-                textday1 = body.forecast.forecastday.condition.text;
-                session.send(city_proper + ": " + dateday1 + ", " + tempday1 + ", " + textday1);
+                dateday1 = body.forecast.forecastday.[0].date;
+                tempday1 = body.forecast.forecastday.[0].day.maxtemp_c;
+                textday1 = body.forecast.forecastday.[0].condition.text;
+                session.send(city_proper + ": " + dateday1 + ", " + tempday1 + "C, " + textday1);
             });
         } else {
             builder.Prompts.text(session, 'Which city do you want the weather for?');
